e1ca446c66604a889aa2b8dc73aa5d96cb19fef5
Micah Halter <micah.halter@gtri.gatech.edu>
2024-10-31 08:33:54 -0400

    feat(aws.lambda): add `ETLRecord` and `BucketNotificationRecord` entries

--- capepy/aws/lambda_.py
+++ capepy/aws/lambda_.py
@@ -1,11 +1,12 @@
 import json
+from urllib.parse import unquote_plus
 
 
 class Record(object):
     """An object for general records in AWS Lambda handlers.
 
     Attributes:
-        body: The body of the record parsed as JSON
+        raw: The raw record
     """
 
     def __init__(self, record):
@@ -14,7 +15,50 @@ class Record(object):
         Args:
             record (object): A record from an AWS Lambda handler event
         """
-        self.body = json.loads(record["body"])
+        self.raw = record
+
+
+class BucketNotificationRecord(Record):
+    """An object for S3 bucket notification related records passed into AWS Lambda handlers.
+
+    Attributes:
+        bucket: The name of the bucket
+        key: The key into the bucket if relevant
+    """
+
+    def __init__(self, record):
+        """Constructor for instantiating a new record of S3 bucket information
+
+        Args:
+            record (object): An S3 bucket related record from an AWS Lambda handler event
+        """
+        super().__init__(record)
+        self.bucket = self.raw["s3"]["bucket"]["name"]
+        self.key = unquote_plus(
+            self.raw["s3"]["object"]["key"], encoding="utf-8"
+        )
+
+
+class ETLRecord(Record):
+    """An object for ETL related records passed into AWS Lambda handlers.
+
+    Attributes:
+        job: The name of the ETL Job
+        bucket: The name of the bucket
+        key: The key into the bucket if relevant
+    """
+
+    def __init__(self, record):
+        """Constructor for instantiating a new record of S3 bucket information
+
+        Args:
+            record (object): An S3 bucket related record from an AWS Lambda handler event
+        """
+        super().__init__(record)
+        body = json.loads(self.raw["body"])
+        self.job = body["etl_job"]
+        self.bucket = body["bucket"]
+        self.key = body["key"]
 
 
 class PipelineRecord(Record):
@@ -33,25 +77,7 @@ class PipelineRecord(Record):
             record (object): An analysis pipeline related record from an AWS Lambda handler event
         """
         super().__init__(record)
-        self.name = self.body["pipeline_name"]
-        self.version = self.body["pipeline_version"]
-        self.parameters = self.body["parameters"]
-
-
-class BucketRecord(Record):
-    """An object for S3 bucket related records passed into AWS Lambda handlers.
-
-    Attributes:
-        name: The name of the bucket
-        key: The key into the bucket if relevant
-    """
-
-    def __init__(self, record):
-        """Constructor for instantiating a new record of S3 bucket information
-
-        Args:
-            record (object): An S3 bucket related record from an AWS Lambda handler event
-        """
-        super().__init__(record)
-        self.name = self.body["bucket_name"]
-        self.key = self.body["key"] if "key" in self.body else None
+        body = json.loads(self.raw["body"])
+        self.name = body["pipeline_name"]
+        self.version = body["pipeline_version"]
+        self.parameters = body["parameters"]
